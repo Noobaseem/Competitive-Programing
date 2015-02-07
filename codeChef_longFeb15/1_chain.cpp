3f1679e1b185799f67ff85df3ce472d0853c6750
Noobaseem <aseem.rastogi1992@gmail.com>
2015-02-07 21:11:43 +0530

    successful submission :D

--- codeChef_longFeb15/1_chain.cpp
+++ codeChef_longFeb15/1_chain.cpp
@@ -31,46 +31,26 @@ using namespace std;
 /*Problem specific declarations*/
 char availChars[2] = {'-','+'};
 
-char expectedChar(char current){
-	return (current == availChars[0] ? availChars[1] : availChars[0]);
-}
-
 int main(int argc, char* argv[]){
 	int T;
 	scanf("%d", &T);
 	while(T--){
 		char input[100005];
 		scanf("%s",&input);
-		char inputCopy[100005];
-		strcpy(inputCopy, input);
-		char next, nextRev, expected, expectedRev;
 		int length 	= strlen(input);
 		int counter	= 0;
 		int switches = 0, switchesRev = 0;
+		char startWith  = '+';
+		char startWith2 = '-';
 		while(length--){
-			expected 	= expectedChar(input[counter]);
-			expectedRev = expectedChar(inputCopy[length-1]); 
-			if(strlen(input) == 1){
-				break;
+			if(input[counter] != startWith){
+				switches++;
 			}
-			if(counter <= strlen(input)-2){
-				next 	= input[counter+1];
-				nextRev = inputCopy[length-2];
-				
-				if(next == expected){
-					//do nothing
-				}else{
-					switches++;
-					input[counter+1] = expected;
-				}
-	
-				if(nextRev == expectedRev){
-					//Do nothing
-				}else{
-					switchesRev++;
-					inputCopy[length-2] = expectedRev;
-				}
+			startWith = startWith == '+' ? '-' : '+';
+			if(input[counter] != startWith2){
+				switchesRev++;
 			}
+			startWith2 = startWith2 == '+' ? '-' : '+';
 			counter++;
 		}
 		if(switches > switchesRev){
