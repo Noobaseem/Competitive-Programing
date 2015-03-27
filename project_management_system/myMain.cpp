c26f757c4a98e1764c2a96b762d6dfaec283ea0d
Noobaseem <aseem.rastogi1992@gmail.com>
2015-03-27 14:23:57 +0530

    Project management project in c++ and mysql

---
+++ project_management_system/myMain.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <my_global.h> 		// Include this file first to avoid problems
+#include <mysql.h> 			// MySQL Include File
+
+#define SERVER "localhost"
+#define USER "username"
+#define PASSWORD "password"
+#define DATABASE "databasename"
+
+using namespace std;
+
+int main(){
+	cout<<"HelloWorld";
+	return 0;
+}
