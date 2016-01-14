80916d31a50016b2e0f97a9bac213b48b2270f35
Noobaseem <aseem.rastogi1992@gmail.com>
2016-01-14 17:14:58 +0530

    adding the constants file

---
+++ learning/getsetgo/src/main/java/in/logikx/services/PreparationConstants.java
@@ -0,0 +1,13 @@
+package in.logikx.services;
+
+/**
+ * Defining the constants that can be used throughout the project
+ * 
+ * @author aseem
+ *
+ */
+public abstract class PreparationConstants {
+	
+	public static final Integer STACK_SIZE = 10;
+	
+}
