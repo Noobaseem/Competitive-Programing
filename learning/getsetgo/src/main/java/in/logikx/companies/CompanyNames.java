69dc3d76e520a188e8ef99a0acb196f19d12f714
Noobaseem <aseem.rastogi1992@gmail.com>
2016-01-14 18:26:16 +0530

    Adding the name of companies i fancy

---
+++ learning/getsetgo/src/main/java/in/logikx/companies/CompanyNames.java
@@ -0,0 +1,14 @@
+package in.logikx.companies;
+
+/**
+ * Here is the list of top IT companies that one desires to work for.
+ * 
+ * @author aseem
+ *
+ */
+public abstract class CompanyNames {
+	public static final String FLIPKART = "flipkart";
+	public static final String AMAZON = "amazon";
+	public static final String GOOGLE = "google";
+	public static final String MICROSOFT = "microsoft";
+}
