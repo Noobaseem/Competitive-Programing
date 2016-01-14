9a070604272a31c45c5fba7df3d151c4140ae131
Noobaseem <aseem.rastogi1992@gmail.com>
2016-01-14 18:11:37 +0530

    Adding the Pair strucuture just like the one found in C++ stl

---
+++ learning/getsetgo/src/main/java/in/logikx/datastructures/Pair.java
@@ -0,0 +1,42 @@
+package in.logikx.datastructures;
+
+/**
+ * There does not exist any datastructure for pair in the java util package.
+ * I loved the c++ Pair structure and thought of implementing the same in java.
+ * 
+ * 
+ * @author aseem
+ *
+ * @param <L>
+ * @param <R>
+ */
+public class Pair<L,R> {
+	
+	private L left;
+	private R right;
+	
+	public Pair(L left, R right){
+		this.left = left;
+		this.right = right;
+	}
+	
+	public L getLeft(){
+		return this.left;
+	}
+	
+	public R getRight(){
+		return this.right;
+	}
+	
+	@Override
+	public int hashCode(){
+		return this.left.hashCode()^this.right.hashCode();
+	}
+	
+	@Override
+	public boolean equals(Object o){
+		if (!(o instanceof Pair)) return false;
+		Pair pair = (Pair) o;
+		return this.left.equals(pair.getLeft()) && this.right.equals(pair.getRight());
+	}
+}
