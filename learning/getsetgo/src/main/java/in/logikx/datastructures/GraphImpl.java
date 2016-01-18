fbb174daeb0c8567c1817a4473ce522f8ab2a7b4
Noobaseem <aseem.rastogi1992@gmail.com>
2016-01-18 16:24:35 +0530

    Architecture for graph api

--- learning/getsetgo/src/main/java/in/logikx/datastructures/GraphImpl.java
+++ learning/getsetgo/src/main/java/in/logikx/datastructures/GraphImpl.java
@@ -6,6 +6,44 @@ package in.logikx.datastructures;
  * @author aseem
  *
  */
-public class GraphImpl {
+public class GraphImpl extends Graph {
 
+	public GraphImpl(int nodes, boolean directed) {
+		super(nodes, directed);
+	}
+
+	public void addEdge(int source, int target, int weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addEdgeSafe(int source, int targer, int weight) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addEdge(Edge e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void addEdgeSafe(Edge e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeAllEdges() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void removeEdge(int source, int edge) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	public void getWeightOfEdge(int source, int edge) {
+		// TODO Auto-generated method stub
+		
+	}
 }
