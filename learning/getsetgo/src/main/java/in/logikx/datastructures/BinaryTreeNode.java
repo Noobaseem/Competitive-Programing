4e894c343640705d5b4eb3cca18f73dc7513abdf
Noobaseem <aseem.rastogi1992@gmail.com>
2016-01-18 14:36:22 +0530

    Adding some getters and setters

--- learning/getsetgo/src/main/java/in/logikx/datastructures/BinaryTreeNode.java
+++ learning/getsetgo/src/main/java/in/logikx/datastructures/BinaryTreeNode.java
@@ -7,7 +7,32 @@ package in.logikx.datastructures;
  * @param <E>
  */
 public class BinaryTreeNode<E> {
+	
 	E value = null;
 	BinaryTreeNode<E> left = null;
 	BinaryTreeNode<E> right = null;
+	
+	public E getValue() {
+		return value;
+	}
+	
+	public void setValue(E value) {
+		this.value = value;
+	}
+	
+	public BinaryTreeNode<E> getLeft() {
+		return left;
+	}
+
+	public void setLeft(BinaryTreeNode<E> left) {
+		this.left = left;
+	}
+
+	public BinaryTreeNode<E> getRight() {
+		return right;
+	}
+
+	public void setRight(BinaryTreeNode<E> right) {
+		this.right = right;
+	}
 }
